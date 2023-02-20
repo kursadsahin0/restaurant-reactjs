b687e2e2db7fa5248cf587b405679417e6df2150
Kürşat <90332216+kursadsahin0@users.noreply.github.com>
2023-02-20 13:14:20 +0300

    Update Slider.jsx

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -6,7 +6,6 @@ function Slider() {
             <img src='/pasta.jpg' width="100%" height="600px" alt='pasta' className='image' />
             <h1 className='title'>FRESH <span>FOOD IN THE</span> <br /> MORNING</h1>
             <p className='description'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia alias eligendi repellendus laudantium odio repudiandae quo consequuntur aliquid. Odio ipsum magnam quos consequuntur in, dolor maiores iusto quas delectus aperiam!</p>
-//             <button className='buttons'>Get Yours Now</button>
         </div>
     )
 }
